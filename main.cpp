398587770502a5081c4872d2019ab8a8c8304f52
kevin <nkevin245@csu.fullerton.edu>
2016-10-20 16:00:53 -0700

    Lamp Class

--- main.cpp
+++ main.cpp
@@ -1,27 +1,79 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 class lamp
 {
+	private:
+	color lampColor;
+	int length, width;
+	int area;
 
-enum color{yellow, white}
+	public:
+	bool isOn(string answer);
+	void lamp_light();
 
-cout << "What color is the lamp?  << endl;
-cin >> color
-cout << "The lamp is " << color << ".\n"
+	cout << "What is the length of the lamp?";
+	cin >> length;
 
-cout << "Do you want the lamp to turn on or off?" << endl;
+	cout << "What is the width of the lamp?";
+	cin >> width;
 
-bool isOn = false;
-void ButtonPress()
-{
-	isOn = !isOn;
-	if(isOn)
-		TurnLampOn();
-	if(!isOn)
-		TurnLampOff();
+	int area() {return length * width};
 }
 
+bool lamp::isOn(string answer)
+{
+	if (answer == "Yes" || answer == "yes")
+		return true;
+	else if (answer == "No" || answer == "no")
+		return false;
+}
+
+void lamp::lamp_color()
+{
+	string color;
+
+	cout << "Would you like the lamp to be White, Blue, Black, or Purple?"
+	cin >> color
+
+	if (color == "White" || color == "white")
+	{
+		cout << "You have chosen a white lamp." << break;
+	}
+	else if (color == "Blue" || color == "blue")
+	{
+		cout << "You have chosen a blue lamp." << break;
+	}
+	else if (color == "Black" || color == "black")
+	{
+		cout << "You have chosen a black lamp." << break;
+	}
+	else if (color == "Purple" || color == "purple")
+	{
+		cout << "You have chosen a purple lamp." << break;
+	}
+	else
+	{
+		cout << "Please choose the according colors from the list." << continue;
+	}
+}
+
+void lamp::lamp_light()
+{
+	string answer;
+
+	cout << "Would you like the lamp to be on?";
+	cin >> answer;
+
+	bool isItOn == isOn(answer);
+
+	if (isItOn == true)
+		cout << "The lamp is turned on." << endl;
+	else
+		cout << "The lamp is turned off." << endl;
+}
+
+int main()
+{
+	return 0;
 }

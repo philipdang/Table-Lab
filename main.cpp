eed544d0b16b21aabaf818608563384e43e42038
Gabriel Rivera <gabriel-rivera-457@csu.fullerton.edu>
2016-10-21 16:27:44 -0700

    Updated Main.cpp w/ Book and Tissue Box Classesø

--- main.cpp
+++ main.cpp
@@ -12,6 +12,21 @@ class book;
 class tissueBox;
 class lamp;
 
+/* 
+-----
+Main
+-----
+*/
+int main(void)
+{
+
+}
+
+/*
+-----
+Table Class
+-----
+*/
 class table
 {
 private:
@@ -54,6 +69,79 @@ public:
     void output_areas(book aBook, tissueBox box, lamp aLamp, bool huhBook, bool huhBox, bool huhLamp);
 };
 
+/*
+-----
+Book Class
+-----
+*/
+class book
+{
+private:
+    enum isOpen {open, closed};
+    
+    isOpen isItOpen;
+    int numPages, length, width;
+    int area = get_area();
+    string title;
+    
+public:
+    int number_of_pages() const{return numPages;}
+    string get_title() const{return title;}
+    
+    void get_length()
+    {
+        cout << "What is the length of the book? ";
+        cin >> length;
+    }
+    void get_width()
+    {
+        cout << "What is the width of the book? ";
+        cin >> width;
+    }
+    int get_area() {return length * width;}
+    
+    void is_book_open();
+};
+
+/*
+-----
+Tissue Box Class
+-----
+*/
+class tissueBox
+{
+private:
+    int numTissues, currentTissues = 100 /* max */, maxTissues = 100, length, width;
+    int area = get_area();
+
+public:
+    int number_of_tissues() const{return numTissues;}
+    int current_tissues() const{return currentTissues;}
+    
+    void get_length()
+    {
+        cout << "What is the length of the tissue box? ";
+        cin >> length;
+    }
+    void get_width()
+    {
+        cout << "What is the width of the tissue box? ";
+        cin >> width;
+    }
+    void use_tissue()
+    {
+        currentTissues = currentTissues - 1;
+    }
+    void wanna_use_tissue(tissueBox box);
+    
+    int get_area() {return length * width;}
+};
+
+/*
+-----
+Functions From Table Class
+-----
+*/
 void table::add_book(book aBook, bool huh)
 {
     char response;
@@ -70,7 +158,7 @@ void table::add_book(book aBook, bool huh)
             aBook.is_book_open();
             aBook.get_area();
             break;
-        }
+       }
         else if (response == 'N' || response == 'n')
         {
             break;
@@ -343,3 +431,90 @@ void table::output_areas(book aBook, tissueBox box, lamp aLamp, bool huhBook, bo
         cout << "The lamp is using " << aLamp.get_area() << " space." << endl;
     }
 }
+
+/*
+-----
+Functions From Book Class
+-----
+*/
+void book::is_book_open()
+{
+    string temp;
+    while(true)
+    {
+        cout << "Is the book open or closed? ";
+        cin >> temp;
+        
+        if (temp == "Open" || temp == "open")
+        {
+            isItOpen = open; break;
+            
+        }
+        else if (temp == "Closed" || temp == "closed")
+        {
+            isItOpen = closed;
+            break;
+        }
+        else
+            cout << "Please answer only 'open' or 'closed.'" << endl; continue;
+    }
+    
+    if (isItOpen == open)
+    {
+        length = length * 2;
+    }
+}
+
+/*
+-----
+Functions From Tissue Box Class
+-----
+*/
+void tissueBox::wanna_use_tissue(tissueBox box)
+{
+    if (currentTissues > 0)
+    {
+        while(true)
+        {
+            char temp;
+            cout << "Wanna use a tissue(y/n)? ";
+            cin >> temp;
+            cout << endl;
+            
+            if (temp == 'Y' || temp == 'y')
+            {
+                box.use_tissue();
+            }
+            else if (temp == 'N' || temp == 'n')
+            {
+                break;
+            }
+            else
+                cout << "Please answer Y or N only." << endl;
+            
+        }
+        while(true)
+        {
+            char temp;
+            cout << "Wanna use a another tissue(y/n)? ";
+            cin >> temp;
+            cout << endl;
+            
+            if (temp == 'Y' || temp == 'y')
+            {
+                box.use_tissue();
+            }
+            else if (temp == 'N' || temp == 'n')
+            {
+                break;
+            }
+            else
+                cout << "Please answer Y or N only." << endl;
+            
+        }
+        
+    }
+    else
+        cout << "You have run out of tissues." << endl;
+}
+
